d092a68853e5890d644507a12dd264b00a8803ec
Vivek Singh <viveksingh9301@gmail.com>
2026-06-04 21:28:54 +0530

    Add GSAP animations to grid items in app.js and include GSAP library in index.html

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ const lenis = new Lenis({
 });
 
 const gridItems = document.querySelectorAll(".grid__item");
+
+for (const item of gridItems) {
+  const xTransform = gsap.utils.random(-100, 100);
+
+  gsap
+    .timeline()
+    .set(item, {
+      transformOrigin: `${xTransform < 0 ? 0 : "100%"}`,
+    })
+    .to(item, {
+      scale: 0,
+      ease: "none",
+    });
+}
