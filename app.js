c770af1fdc9488d79c251e919399805548fd6b6a
Vivek Singh <viveksingh9301@gmail.com>
2026-06-06 10:46:07 +0530

    Refactor grid item animations in app.js to improve scaling and horizontal movement effects with GSAP and ScrollTrigger

--- app.js
+++ app.js
@@ -7,31 +7,32 @@ gsap.registerPlugin(ScrollTrigger);
 
 const gridItems = document.querySelectorAll(".grid__item");
 
-for (const item of gridItems) {
+gridItems.forEach((item) => {
   const xTransform = gsap.utils.random(-100, 100);
 
-  gsap
-    .timeline()
-    .set(item, {
-      transformOrigin: `${xTransform < 0 ? 0 : "100%"}`,
-    })
-    .to(item, {
-      scale: 0,
-      ease: "none",
-      scrollTrigger: {
-        trigger: item,
-        start: "top top",
-        end: "bottom top",
-        scrub: true,
-      },
-    })
-    .to(item, {
-      xPercent: xTransform,
-      scrollTrigger: {
-        trigger: item,
-        start: "top bottom",
-        end: "bottom top",
-        scrub: true,
-      },
-    });
-}
+  gsap.set(item, {
+    transformOrigin: xTransform < 0 ? "0% 50%" : "100% 50%",
+  });
+
+  gsap.to(item, {
+    scale: 0,
+    ease: "none",
+    scrollTrigger: {
+      trigger: item,
+      start: "top top",
+      end: "bottom top",
+      scrub: true,
+    },
+  });
+
+  gsap.to(item, {
+    xPercent: xTransform,
+    ease: "none",
+    scrollTrigger: {
+      trigger: item,
+      start: "top bottom",
+      end: "bottom top",
+      scrub: true,
+    },
+  });
+});
